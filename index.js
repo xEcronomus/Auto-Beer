2b2c684122a48c2aac1cd70c32157198b3d43f98
xEcronomus <zoli.0843@gmail.com>
2018-09-12 16:24:39 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const Command = require('command');
 
 module.exports = function auto_beer(dispatch) {
-	var bUSE_BROOCH = true; // SET THIS TO false IF YOU DONT WANT AUTO BROOCH
-	var bUSE_BEER = false; // SET THIS TO false IF YOU DONT WANT AUTO BEER 
-	var bENABLED = true;
-	
-	
-	
-	
-	
+	var bUSE_BROOCH	= false; 
+	var bUSE_BEER	= false;
+	var bENABLED	= false;
+	var bDebug		= false;
+
 	const command = Command(dispatch);
 	const ROOT_BEER = 80081;
 	const BLACKLIST = [19701,19704,19735]; // 1-2Not useable brooches,Clensing brooch, 
-	const BUFF = [268635656,67438964,67228964,67449064,67309164];//BUFF skill Deadly_Gamble, Unleash, Ragnarok, Manaboost, In Cold Blood
 	let MyGameId = null;
 	let lastLocation = null;
-	var myClass = null;
-	var msg = '';
-	
+	var myClass = null;	
 	var current_Brooch = null;
+	
 	///////////////////////////////////////////////////////////
-	const DEADLY_GAMBLE = 268635656;
-	const UNLEASH 		= 67438964;
-	const MANA_BOOST 	= 67449064;
-	const RAGNAROK 	 	= 67228964;
+	const DEADLY_GAMBLE = 268635656;//WARRIOR
+	const UNLEASH 		= 268765556;//ZERK
+	const MANA_BOOST 	= 268775656;//SORC
+	const RAGNAROK 	 	= 268555556;//VALK
+	const ARUSH 		= 11111111;//LANCER
+	const ICB			= 268635756;//SLAYER
+	const SREAPING		= 11111111;//REAPER
+	//////AWAKENING PART 2 SKILLS/////////////////////////////
+	const MBREEZE		= 11111111;//ARCHER
+	const BEAM_PANEL	= 11111111;//GUNNER
+	const ENLIGHTENMENT	= 11111111;//NINJA
 	///////////////////////////////////////////////////////////
-	const VALK_ID 	 = 10813;
-	const ZERK_ID 	 = 10804;
-	const WARRIOR_ID = 10801;
-	const SORC_ID	 = 10805;
-	///////////////////////////////////////////////////////////
+	
 	dispatch.hook('S_LOGIN', 9, event => { 
 		MyGameId = event.gameId;
 		myClass = event.templateId;
@@ -44,63 +42,118 @@ module.exports = function auto_beer(dispatch) {
 		}
 	});
 	dispatch.hook('S_ACTION_STAGE', 3, (event) => {	
-		
-		/*if(event.gameId.equals(MyGameId)){
-			switch(myClass.toString().slice(-2)) {
-				case 13://VALK_ID
-				if(event.skill.equals(RAGNAROK))
-				{
-					//USE
-				}
-					break;
-				case 04://ZERK_ID
-				
-						break;
-				case 01://WARRIOR_ID
-				
-					break;
-				case 05://SORC_ID
-				
-					break;
-				default:
-			
-			}	
-		}*/
-
 		if(event.gameId.equals(MyGameId)){
-			if(BUFF.includes(event.skill)){
-				if(bENABLED){
-					if(myClass.toString()!='10806'){
-						if(bUSE_BROOCH){useBrooch();}
-						if(bUSE_BEER){useBeer();}
+			if(bDebug){command.message(event.skill.toString());}
+			switch(myClass.toString().slice(-2)) {
+				case '13'://VALK
+					if(event.skill==RAGNAROK)
+					{
+						//command.message("ragnarok used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
 					}
-				}
-			}
+					break;
+				case '04'://ZERK
+					if(event.skill==UNLEASH)
+					{
+						//command.message("unlesh used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				case '01'://WARRIOR
+					if(event.skill==DEADLY_GAMBLE)
+					{
+					//	command.message("Deadly Gamble used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				case '05'://SORC
+					if(event.skill==MANA_BOOST)
+					{
+						//command.message("mana boost used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				case '02'://LANCER
+					if(event.skill==ARUSH)
+					{
+						//command.message("ARUSH used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				case '03'://SLAYER
+					if(event.skill==ICB)
+					{
+						//command.message("ICB used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				case '06'://ARCHER
+					if(event.skill==MBREEZE)
+					{
+						//command.message("Melodic Breeze used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				case '09'://Reaper
+					if(event.skill==SREAPING)
+					{
+						//command.message("Shadow reaping used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				case '10'://Gunner
+					if(event.skill==BEAM_PANEL)
+					{
+						//command.message("Beam Panel used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				case '12'://Ninja
+					if(event.skill==ENLIGHTENMENT)
+					{
+						//command.message("ENLIGHTENMENT used");
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}
+					}
+					break;
+				
+				default:
+					command.message("No class found S_ACTION_STAGE HOOK");
+					break;				
+			}	
 		}
-		
-	});
-	///////////////////////////////////////////////////////////
-	command.add('ab', () => {
-		bENABLED = !bENABLED;
-		if(bENABLED){msg = 'Autobeer enabled';}else{msg = 'Autobeer disabled';}
-		command.message(msg);
-	});
-	command.add('abbeer', () => {
-		bUSE_BEER = !bUSE_BEER;
-		if(bUSE_BEER){msg = 'Beer enabled';}else{msg = 'Beer disabled';}
-		command.message(msg);
-	});
 	
-	command.add('abbrooch', () => {
-		bUSE_BROOCH = !bUSE_BROOCH;	
-		if(bUSE_BROOCH){msg = 'Brooch enabled';}else{msg = 'Brooch disabled';}
-		command.message(msg);
+	});//S_ACTION_STAGE END 
+	///////////////////////////////////////////////////////////	
+	command.add('ab0', () => {
+		bENABLED 	= false;
+		bUSE_BEER 	= false;
+		bUSE_BROOCH = false;
+		command.message('Auto beer disabled');
 	});
-	command.add('abinfo', () => {
-		msg = myClass.toString();
-		command.message(msg);
-	///////////////////////////////////////////////////////////
+	command.add('ab1', () => {
+		bENABLED 	= true;
+		bUSE_BEER 	= false;
+		bUSE_BROOCH = true;
+		command.message('Only brooch enabled');
 	});
+	command.add('ab2', () => {
+		bENABLED 	= true;
+		bUSE_BEER 	= true;
+		bUSE_BROOCH = false;
+		command.message('Only Beer enabled');
+	});
+	command.add('ab3', () => {
+		bENABLED 	= true;
+		bUSE_BEER 	= true;
+		bUSE_BROOCH = true;
+		command.message('Brooch+Beer enabled');
+	});
+	command.add('abdebug', () => {
+		bDebug = !bDebug;
+		command.message('Debug mode');
+		command.message(String(bDebug));
+	});
+	/////////////////////////////////////////////////////////
 	function useBeer(){
 		dispatch.toServer('C_USE_ITEM', 3, {
 				gameId: MyGameId,
@@ -109,7 +162,7 @@ module.exports = function auto_beer(dispatch) {
 				loc: lastLocation.loc,
 				w: lastLocation.w,
 				unk4: true
-	});
+		});
 	}
 	function useBrooch(){
 		if(!BLACKLIST.includes[current_Brooch]){
@@ -123,5 +176,4 @@ module.exports = function auto_beer(dispatch) {
 			});
 		}
 	}
-
 }
