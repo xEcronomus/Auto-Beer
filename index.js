23a1cad3b1645dfe88ef1c339b9ec17c5cea9547
xEcronomus <zoli.0843@gmail.com>
2018-12-31 19:29:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,90 @@
-
-const Command = require('command');
-
-module.exports = function auto_beer(dispatch) {
+module.exports = function auto_beer(mod) {
 	var bUSE_BROOCH	= false; 
 	var bUSE_BEER	= true;
 	var bENABLED	= true;
 
-	const command = Command(dispatch);
+
 	const ROOT_BEER = 80081;
 	const BLACKLIST = [19701,19704,19735]; // 1-2Not useable brooches,Clensing brooch, 
+	const SORCBLACKLIST = [268505556,268745566,268695556,268616056,268635956]; 
+	
 	let gameId = null;
 	let lastLocation = null;
-	var currentClass = null;	
-	var currentBrooch = null;
+	var myClass = null;	
+	var current_Brooch = null;
 
-	const DEADLY_GAMBLE 	= 268635656;//WARRIOR
-	const UNLEASH 		= 268765556;//ZERK
-	const MANA_BOOST 	= 268775656;//SORC
-	const RAGNAROK 	 	= 268555556;//VALK
-	const RAGNAROK2	 	= 268685556;//VALK
-	const ARUSH 		= 268605656;//LANCER
-	const ICB		= 268635756;//SLAYER
-	const SREAPING		= 268595556;//REAPER
-	const MBREEZE		= 268785556;//ARCHER
-	const BEAM_PANEL	= 268845557;//GUNNER
-	const ENLIGHTENMENT	= 268665556;//NINJA
+	const DEADLY_GAMBLE = 200200;//WARRIOR
+	const UNLEASH 		= 330100;//ZERK   268765556
+	const MANA_BOOST 	= 340200;//SORC
+	const RAGNAROK 	 	= 120100;//VALK
+	const RAGNAROK2	 	= 250100;//VALK
+	const ARUSH 		= 170200;//LANCER
+	const ICB			= 200300;//SLAYER
+	const SREAPING		= 230100;//REAPER
+
+	const MBREEZE		= 350100;//ARCHER
+	const BEAM_PANEL	= 410101;//GUNNER
+	const ENLIGHTENMENT	= 230100;//NINJA
 	
-	dispatch.hook('S_LOGIN', 9, event => { 
+	mod.hook('S_LOGIN', 12, event => { 
 		gameId = event.gameId;
-		currentClass = event.templateId;
+		myClass = event.templateId;
 	});
-	dispatch.hook('C_PLAYER_LOCATION', 3, event => { lastLocation = event })
-	dispatch.hook('S_INVEN', 14, event => {
+	mod.hook('C_PLAYER_LOCATION', 5, event => { lastLocation = event })
+	mod.hook('S_INVEN', 16, event => {
 		for(let item of event.items) {    
 			if(item.slot === 20) {
-				currentBrooch = item.id;
+				current_Brooch = item.id;
 				break;
 			}
 		}
+
 	});
-	dispatch.hook('S_ACTION_STAGE', 3, {order: -50}, (event) => {	
-		if(event.gameId.equals(gameId)){
-			switch(currentClass.toString().slice(-2)) {
+	
+	mod.hook('S_ACTION_STAGE', 9,{order: -50}, (event) => {	
+		if(event.gameId==gameId){
+			//mod.command.message(event.skill.id);
+			switch(myClass.toString().slice(-2)) {
 				case '13'://VALK
-					if(event.skill==RAGNAROK){
+					if(event.skill.id==RAGNAROK){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
-					if(event.skill==RAGNAROK2){
-						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}	
+					if(event.skill.id==RAGNAROK2){
+						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				case '04'://ZERK
-					if(event.skill==UNLEASH){
+					if(event.skill.id==UNLEASH){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				case '01'://WARRIOR
-					if(event.skill==DEADLY_GAMBLE){
+					if(event.skill.id==DEADLY_GAMBLE){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
-				case '05'://SORC
-					if(event.skill==MANA_BOOST){
+				case '05'://SORC	
+					if(event.skill.id==MANA_BOOST){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				case '02'://LANCER
-					if(event.skill==ARUSH){
+					if(event.skill.id==ARUSH){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				case '03'://SLAYER
-					if(event.skill==ICB){
+					if(event.skill.id==ICB){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				case '06'://ARCHER
-					if(event.skill==MBREEZE){
+					if(event.skill.id==MBREEZE){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				case '09'://Reaper
-					if(event.skill==SREAPING){
+					if(event.skill.id==SREAPING){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				case '10'://Gunner
-					if(event.skill==BEAM_PANEL){
+					if(event.skill.id==BEAM_PANEL){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				case '12'://Ninja
-					if(event.skill==ENLIGHTENMENT){
+					if(event.skill.id==ENLIGHTENMENT){
 						if(bENABLED){if(bUSE_BROOCH){useBrooch();}if(bUSE_BEER){useBeer();}}}
 					break;
 				default:
@@ -89,32 +92,39 @@ module.exports = function auto_beer(dispatch) {
 			}	
 		}
 	});
-	command.add('ab0', () => {
+	mod.command.add('ab0', () => {
 		bENABLED 	= false;
 		bUSE_BEER 	= false;
 		bUSE_BROOCH = false;
-		command.message('Auto beer disabled');
+		mod.command.message('Auto beer disabled');
 	});
-	command.add('ab1', () => {
+	
+	mod.command.add('ab1', () => {
 		bENABLED 	= true;
 		bUSE_BEER 	= false;
 		bUSE_BROOCH = true;
-		command.message('Only brooch enabled');
+		mod.command.message('Only brooch enabled');
 	});
-	command.add('ab2', () => {
+	mod.command.add('ab2', () => {
 		bENABLED 	= true;
 		bUSE_BEER 	= true;
 		bUSE_BROOCH = false;
-		command.message('Only Beer enabled');
+		mod.command.message('Only Beer enabled');
 	});
-	command.add('ab3', () => {
+	mod.command.add('ab3', () => {
 		bENABLED 	= true;
 		bUSE_BEER 	= true;
 		bUSE_BROOCH = true;
-		command.message('Brooch+Beer enabled');
+		mod.command.message('Brooch+Beer enabled');
+	});
+	mod.command.add('usebeer', () => {
+		useBeer();
+	});
+	mod.command.add('usebrooch', () => {
+		useBrooch();
 	});
 	function useBeer(){
-		dispatch.toServer('C_USE_ITEM', 3, {
+		mod.send('C_USE_ITEM', 3, {
 				gameId: gameId,
 				id: ROOT_BEER,
 				amount: 1,
@@ -124,10 +134,10 @@ module.exports = function auto_beer(dispatch) {
 		});
 	}
 	function useBrooch(){
-		if(!BLACKLIST.includes[currentBrooch]){
-			dispatch.toServer('C_USE_ITEM', 3, {
+		if(!BLACKLIST.includes[current_Brooch]){
+			mod.send('C_USE_ITEM', 3, {
 				gameId: gameId,
-				id: currentBrooch,
+				id: current_Brooch,
 				amount: 1,
 				loc: lastLocation.loc,
 				w: lastLocation.w,
